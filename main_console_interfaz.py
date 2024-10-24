2b51adee5d186c819be6ed5703a26f38b69e199a
Santiago1031 <sancordobafuentes203@gmail.com>
2024-10-24 00:10:22 -0500

    Refactoring

--- main_console_interfaz.py
+++
@@ -1,7 +0,0 @@
-from kivy.app import App
-
-# Importa la clase PensionCalculatorApp desde el archivo pension_interface.py
-from src.gui.gui_pension import PensionCalculatorApp
-
-if __name__ == '__main__':
-    PensionCalculatorApp().run()
