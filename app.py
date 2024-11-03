1a81f66d6f8d8053617d4d0cc65593313c683d2c
luna1860 <saritaprinmc@gmail.com>
2024-11-02 20:01:02 -0500

    creating html file and updating app.py

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 # Para las aplicaciones web creadas con Flask, debemos importar siempre el modulo 
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 
 # Para poder servir plantillas HTML desde archivos, es necesario importar el modulo render_template
 from flask import render_template, request
+
+# Flask constructor: crea una variable que nos servirá para comunicarle a Flask
+# la configuración que queremos para nuestra aplicación
+app = Flask(__name__)     
+
+# Ruta para el menú principal
+@app.route('/')
+def menu_principal():
+    return render_template('menu_principal.html')
