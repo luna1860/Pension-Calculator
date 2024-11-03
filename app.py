3bdfc38fa41eec0417fa0a417725e61df7571399
luna1860 <saritaprinmc@gmail.com>
2024-11-02 19:16:10 -0500

    updating app.py

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 # Para las aplicaciones web creadas con Flask, debemos importar siempre el modulo 
 from flask import Flask
+
+# Para poder servir plantillas HTML desde archivos, es necesario importar el modulo render_template
+from flask import render_template, request
